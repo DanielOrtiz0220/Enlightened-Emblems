f72cc291de7af47e3b61561e779cb4068d4af6b7
Daniel Ortiz <42874497+DanielOrtiz0220@users.noreply.github.com>
2023-03-26 03:36:09 +0000

    Login Page UI/UX complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Login from './components/Login'
+import React, { useState } from 'react';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Login username={username} password={password} setUsername = {setUsername} setPassword = {setPassword} />
     </main>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export default function Login({ username, password, setUsername, setPassword }) {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform your login logic here, e.g., call an API or update the state.
+    console.log('Username:', username, 'Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
